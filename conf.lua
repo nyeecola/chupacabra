260488cda3e1725841078fc54a812c23cb97bd9f
Italo <italo.niccola@gmail.com>
2016-04-02 00:13:40 -0300

    Add project structure

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.window.title = "Nome Loko"
+    t.window.icon = "assets/icon.png"
+    t.window.fullscreen = false
+    t.window.vsync = false
+    t.window.fsaa = 16
+
+    t.version = "0.10.1"
+end
