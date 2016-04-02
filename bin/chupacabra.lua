19148f8b66e9416cda46cf5cfec65d0e8cfeafee
lucascagustini <lucas.c.agustini@gmail.com>
2016-04-02 05:52:08 -0300

    et+obstaculos

--- bin/chupacabra.lua
+++ bin/chupacabra.lua
@@ -1,29 +1,49 @@
-function defineCC(chupacabra, floor)
-    chupacabra.x = love.graphics.getWidth()/2
-    chupacabra.y = floor
-    chupacabra.v = 0
-    chupacabra.stamina = 0
-    chupacabra.grounded = true
-    chupacabra.img = love.graphics.newImage("assets/chupacabra.png")
+function defineCC(cc, floor)
+    cc.x = love.graphics.getWidth()/2
+    cc.y = floor
+    cc.v = 0
+    cc.stamina = 0
+    cc.cooldown = 0
+    cc.scale = 0.15
+    cc.img = love.graphics.newImage("assets/chupacabra.png")
 end
 
-function moveCC(chupacabra, floor, dt)
-    chupacabra.x = chupacabra.x + chupacabra.stamina * dt
-    chupacabra.y = chupacabra.y - chupacabra.v * dt
-    if chupacabra.y > floor then
-        chupacabra.y = floor
-        chupacabra.v = 0
+function updateCC(cc, floor, dt)
+    cc.x = cc.x + cc.stamina * dt
+    cc.y = cc.y - cc.v * dt
+    if cc.y > floor then
+        cc.y = floor
+        cc.v = 0
     end
-    chupacabra.v = chupacabra.v - 190*dt
+    cc.v = cc.v - 190*dt
+
+    if cc.cooldown > 0 then
+        cc.cooldown = cc.cooldown - dt
+    else
+        cc.cooldown = 0
+    end
 end
 
-function drawCC(chupacabra)
+function drawCC(cc)
     love.graphics.setColor(255, 255, 255)
-    love.graphics.draw(cc.img, cc.x, cc.y, 0, 0.15, 0.15)
+    love.graphics.draw(cc.img, cc.x, cc.y, 0, cc.scale, cc.scale)
 end
 
-function keysCC(k)
-    if k == "up" then
+function keysCC(k, cc)
+    if k == "up" and cc.y >= FLOOR then
         cc.v = 200
     end
+end
+
+function collisionDetectionCC(cc, obstacles)
+    if cc.cooldown <= 0 then
+        for i = 1, #obstacles do
+            if cc.x + cc.img:getWidth()*cc.scale > obstacles[i].x and cc.x + cc.img:getWidth()*cc.scale < obstacles[i].x + obstacles[i].width then
+                if (obstacles[i].y < cc.y and cc.y < obstacles[i].y + obstacles[i].height) or (obstacles[i].y < cc.y + cc.img:getHeight()*cc.scale and cc.y + cc.img:getHeight()*cc.scale < obstacles[i].y + obstacles[i].height) then
+                    cc.stamina = cc.stamina -60
+                    cc.cooldown = 2
+                end
+            end
+       end
+   end
 end
